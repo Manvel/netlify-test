401df1515b79e99aa9e1177dcdcee02b27a2be3d
Manvel Saroyan <saroyanm@gmail.com>
2020-10-19 22:56:52 +0400

    updated

--- config.js
+++ config.js
@@ -60,6 +60,62 @@ const templateData =
     description: "CMS created with the internationalization in mind",
     github: "https://github.com/cmints/website",
     crowdin: `https://crowdin.com/translate/${i18nOptions.crowdin.id}`,
+    navigations: [
+      {
+        path: "documentation",
+        contains: "documentation",
+        stringId: "header-menu-item-docs"
+      },
+      {
+        path: "quick-start",
+        stringId: "header-menu-item-tutorial"
+      },
+      {
+        path: "contribute",
+        contains: "contribute",
+        stringId: "header-menu-item-contribute"
+      }
+    ],
+    secondaryNavigations: {
+      documentation: [
+        {
+          category: "getting-started",
+          titleId: "nav-doc-title-getting-started"
+        },
+        {
+          category: "pages",
+          titleId: "nav-doc-title-pages"
+        },
+        {
+          category: "themes",
+          titleId: "nav-doc-title-themes"
+        },
+        {
+          category: "i18n",
+          titleId: "nav-doc-title-i18n"
+        }
+      ],
+      contribute:[
+        {
+          category: "contribute",
+          titleId: "nav-doc-title-contribute"
+        }
+      ]
+    },
+    footerNavigations: [
+      {
+        path: "https://single.cmints.io",
+        stringId: "footer-menu-item-single-lang"
+      },
+      {
+        path: "https://multi.cmints.io",
+        stringId: "footer-menu-item-single-multi"
+      },
+      {
+        path: "/presentation",
+        stringId: "footer-menu-item-slides"
+      }
+    ]
   }
 };
 
