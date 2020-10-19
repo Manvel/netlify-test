b4ea99b42bc77afaa001c687cb2ea68d4c548899
Manvel Saroyan <saroyanm@gmail.com>
2020-10-19 22:52:48 +0400

    updated

--- config.js
+++ config.js
@@ -52,6 +52,85 @@ else if (argv.dev)
 
 const templateData =
 {
+  site: {
+    protocol,
+    domain,
+    root,
+    title: "CMintS",
+    description: "CMS created with the internationalization in mind",
+    github: "https://github.com/cmints/website",
+    crowdin: `https://crowdin.com/translate/${i18nOptions.crowdin.id}`,
+    localeMap: {
+      "de": {name: "Deutsch", region: "de_DE", crowdin: "en-de"},
+      "en": {name: "English", region: "en_US"},
+      "es-ES": {name: "Español", region: "es_ES", crowdin: "en-es"},
+      "fr": {name: "Français", region: "fr_FR", crowdin: "en-fr"},
+      "hy-AM": {name: "Հայերեն", region: "hy_AM", crowdin: "en-hy"},
+      "it": {name: "Italiano", region: "it_IT", crowdin: "en-it"},
+      "lt": {name: "Lietuvių", region: "lt_LT", crowdin: "en-lt"},
+      "pl": {name: "Polski", region: "pl_PL", crowdin: "en-pl"},
+      "pt": {name: "Português", region: "pt_PT", crowdin: "en-pt"},
+      "ru": {name: "Русский", region: "ru_RU", crowdin: "en-ru"},
+      "zh-CN": {name: "中文", region: "zh_CN", crowdin: "en-zhcn"}
+    },
+    defaultLocale: "en",
+    navigations: [
+      {
+        path: "documentation",
+        contains: "documentation",
+        stringId: "header-menu-item-docs"
+      },
+      {
+        path: "quick-start",
+        stringId: "header-menu-item-tutorial"
+      },
+      {
+        path: "contribute",
+        contains: "contribute",
+        stringId: "header-menu-item-contribute"
+      }
+    ],
+    secondaryNavigations: {
+      documentation: [
+        {
+          category: "getting-started",
+          titleId: "nav-doc-title-getting-started"
+        },
+        {
+          category: "pages",
+          titleId: "nav-doc-title-pages"
+        },
+        {
+          category: "themes",
+          titleId: "nav-doc-title-themes"
+        },
+        {
+          category: "i18n",
+          titleId: "nav-doc-title-i18n"
+        }
+      ],
+      contribute:[
+        {
+          category: "contribute",
+          titleId: "nav-doc-title-contribute"
+        }
+      ]
+    },
+    footerNavigations: [
+      {
+        path: "https://single.cmints.io",
+        stringId: "footer-menu-item-single-lang"
+      },
+      {
+        path: "https://multi.cmints.io",
+        stringId: "footer-menu-item-single-multi"
+      },
+      {
+        path: "/presentation",
+        stringId: "footer-menu-item-slides"
+      }
+    ]
+  }
 };
 
 module.exports = {templateData, markdownOptions, i18nOptions, port,
